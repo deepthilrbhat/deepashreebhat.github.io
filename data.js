006085417b52df02faa96d768b67574e1bf98334
deepthilrbhat <51196288+deepthilrbhat@users.noreply.github.com>
2024-12-02 12:00:48 +0100

    new files
    new files

---
+++ data.js
@@ -0,0 +1,358 @@
+export const bio = [
+  "I am an enthusiastic data analytics leader who is results-driven, self-motivated and creative in solving problems. I am adept at uniting multidisciplinary teams to accomplish common objectives and encouraging team development via mentoring and coaching.",
+  "",
+  "",
+];
+
+export const leadershipPhilosophy = [
+  "Developing a culture of data-driven innovation and cooperation is at the heart of my leadership ethos. I'm dedicated to giving teams the resources, encouragement and strategic direction they need to turn data into useful business insights. I support open communication, transparent leadership and the development of a continuous improvement mindset. I align analytics projects with business goals to deliver measurable results that drive growth, efficiency and informed decision-making across the organization.",
+  "",
+  "",
+];
+
+export const skills = [
+  {
+    title: "Data Visualisation Tools",
+    skillName: "Tableau, Looker Studio, Looker",
+    color: "6",
+    percentage: "80",
+  },
+  {
+    title: "Technical Skills",
+    skillName: "Python, SQL, Java",
+    color: "6",
+    percentage: "80",
+  },
+  {
+    title: "Cloud Platforms",
+    skillName: "GCP",
+    color: "6",
+    percentage: "70",
+  },
+  {
+    title: "Automation Tools",
+    skillName: "Jenkins, Airflow",
+    color: "6",
+    percentage: "70",
+  },
+   {
+    title: "Statistics",
+    skillName: "Statistical Analysis",
+    color: "6",
+    percentage: "70",
+  },
+  {
+    title: "ManagementSkills",
+    skillName: "Startegic Planning",
+    color: "6",
+    percentage: "80",
+  },
+   {
+    title: "ManagementSkills",
+    skillName: "Stakeholder Management",
+    color: "6",
+    percentage: "80",
+  },
+   {
+    title: "ManagementSkills",
+    skillName: "Team building",
+    color: "6",
+    percentage: "80",
+  },
+//   {
+//     title: "Analytics",
+//     skillName: "CleverTap, Countly",
+//     color: "5",
+//     percentage: "50",
+//   },
+//   {
+//     title: "IDE",
+//     skillName: "VS Code",
+//     color: "6",
+//     percentage: "70",
+//   },
+];
+
+export const projects = {
+  project1: 
+    {
+      context: "Business, product and fraud teams were undecided on whether to relax the fraud rules to improve wallet top-up success rate.",
+      challenges:  [`Lack of visibility on the actual root cause`,
+      `Having a buy-in for detailed data analysis`,],
+      approach:[
+         `Clear and open communication on how analytics could help understand the root causes.`,
+      `Customer Journey funnel analysis to understand the conversion rate at each step.`,
+      `Deep dive data analysis to understand the failure reasons and connected customer segments.`,
+      ],
+      impact:  [
+      `Detailed guide on the root causes and what actions could help them improve.`,
+      `The actions resulted in 3x improvement in the Success Rate.`,
+      ],
+      myrole: "Leading, collaboration, planning and regular check-ins",
+    },
+   
+  project2: 
+     {
+      context: "Events tracking was compromised in UI leading to unreliable data insights.",
+      challenges:  [`Multiple teams were responsible for the Events tracking without clear ownership and roles`,
+      ],
+      approach:[
+         `Consulted with individual teams to understand their implementation processes and challenges.`,
+      `Created a unified process by embedding events tracking initiatives at key UI implementation steps and defined clear ownership.`,
+      `Collaborated with cross-functional teams to explain the process and secure buy-in.`,
+      ],
+      impact:  [
+      `Clear ownership and transparent process helped to fix the gaps in events tracking and improved implementation processes.`,
+      `Stakeholders had seamless access to reliable customer insights.`,
+      ],
+      myrole: "Leading, collaboration and planning.",
+    },
+  project3:
+    {
+      context: "A key stakeholder once requested real-time streaming analytics without any details about the underlying problem statement.",
+      challenges:  [`Maintaining stakeholder relationship.`,
+      `Figure out the underlying problem statement and cost/benefit ratio.`,
+      ],
+      approach:[
+         `I quickly assembled the Data Engineering team to estimate costs.`,
+      `Held workshops with business and product stakeholders to understand the use cases where real-time data is necessary.`,
+            `Created a detailed alternate solution plan for the use cases.`,
+      ],
+      impact:  [
+      `Stakeholders had a clear and actionable solution without investing on costly real-time streaming analytics.`,
+      ],
+      myrole: "•  Problem solving, Leading, collaboration and planning",
+    },
+  project4:
+  {
+      context: "A fraud incident occurred where fraudsters were able to spend more than the available balance and it took over a week to identify the incident.",
+      challenges:  [`Lack of availability of daily balance data.`,
+      `Lack of knowledge on balance calculation from ledger entries.`,
+      ],
+      approach:[
+         `Quickly aligned with key stakeholders to understand the problem and possible solutions.`,
+      `Went through the technical implementation (code) to understand the logic of deriving  balance from ledger entries.`,
+      `Quickly built an insightful dashboard to visualize wallet inflows and outflows, highlighting discrepancies for immediate action.`,
+      ],
+      impact:  [
+      `Stakeholders could identify the fraud incidents on time and were able to take immediate action, minimizing monetary losses.`,
+
+      ],
+      myrole: "Problem solving, collaboration and execution",
+    },
+};
+
+export const whatido = {
+  teamBuilding: [
+    "● Identifying the required roles, recruiting the right talent, onboarding, mentoring and coaching team to achieve their full potential.",
+    "● Identifying growth opportunities and determining the skills required for team to succeed.",
+    "● Creating a psychological safe environment where open communication, collaboration and open feedback is valued.",
+    "● Adapting leadership style based on the situation and the individual.",
+
+  ],
+  dataDriven: [
+    "● Understanding the domain, product implementation and stakeholder challenges in depth.",
+    "● Building trustworthy relationships to achieve seamless collaboration with stakeholders.",
+    "● Identifying opportunities to solve business problems by building innovative data products.",
+    "● Translating complex analytics into clear and actionable insights."
+    
+  ],
+  dataManagement: [
+    "● Ensure Data availability, Data completeness, Data quality and Data compliance.",
+    "● Building efficient data models with self-explanatory data catalogues."
+
+  ],
+  projectManagement: [
+    "● Optimizing processes in order to make the team efficient and deliver quality products.",
+    "● Ensure analytics processes and progresses on projects are transparent to stakeholders.",
+    "● Planning and executing small, manageable and achievable projects which translates to a bigger goal."
+
+  ]
+};
+
+export const experience = [
+  {
+    title: "Delivery Hero, Germany",
+    duration: "Apr 2020 - Mar 2024",
+    subtitle: "Senior Analytics Manager (Fintech Product)",
+    details: [
+      `Established and led a proficient Product Analytics Team in Fintech from inception, offering mentorship, essential tools and resources to foster team growth and success.`,
+      `Formulated and implemented a comprehensive roadmap for the wallet analytics function, integrating a clear vision for the future aligned with the company's overarching strategy and vision.`,
+      `Led initiatives to establish robust data management, standardize KPIs, build data products and self-service tools to efficiently overcome cross-functional stakeholder challenges.`,
+      `Orchestrated, supervised and delivered cross-functional projects spanning diverse teams, comprising data engineers, data scientists and data analysts, to produce actionable insights for business growth.`,
+      `Empowered stakeholders to enhance user experience, optimize Conversion Rate (CVR), and improve payment success rate. Orchestrated successful data insights resulting in a significant 4x increase in Success Rate through industry expertise and effective collaboration.`,
+      `Directed initiatives to empower business teams with actionable insights on marketing campaign performance, resulting in a notable increase in wallet payment penetration by approximately 7%.`,
+      
+    ],
+    tags: ["GCP", "SQL", "Python", "Tableau", "LookerStudio", "Looker"],
+    icon: "globe",
+  },
+  {
+    title: "Tink GmbH, Germany",
+    duration: "Sep 2019 - Feb 2020",
+    subtitle: "Business Intelligence Associate",
+    details: [
+      `Led comprehensive end-to-end planning, design, and execution of data platform projects, aligning technical solutions with strategic business objectives.`,
+      `Designed and implemented robust ETL processes to seamlessly collect data from diverse sources, significantly improving data reliability and operational efficiency.`,
+      `Leveraged expertise in SQL, Python, and Jenkins automation to design and deploy BI dashboards and automate reports, empowering informed decision-making.`,
+      
+    ],
+    tags: [
+      "SQL",
+      "Python",
+      "Metabase"
+    ],
+    icon: "globe",
+  },
+  {
+    title: "DBS Bank, Singapore",
+    duration: "Oct 2016 - Aug 2018",
+    subtitle: "Senior Software Consultant",
+    details: [
+      `Responsible for end-to-end delivery of Consumer Banking functionalities such as Unit Trust, Singapore Savings Bond and Share Finance using Agile Software Development Methodology.`,
+      `Worked on requirement gathering from the Product Owner, gap analysis and design/development of the modules.`,
+      `Guiding and mentoring the team members located in different geographies for project implementation and delivery.`,
+      `Involved in all the testing phases, UAT support and production deployment support.`,
+    ],
+    tags: ["JavaScript", "JSP", "AngularJS", "Core Java", "Webservices","SQL","IBM DB2"],
+    icon: "globe",
+  },
+  {
+    title: "SAFRAN Identity & Security, Singapore",
+    duration: "Aug 2014 - Sep 2016",
+    subtitle: "Senior Software Consultant",
+    details: [
+      `Worked on the implementation of Trusted Service Management (TSM) for mobile payments via NFC.`,
+      `Worked on the technical impact analysis, gap analysis for re-engineering the existing functionality and redrawing the new functionality.`,
+      `Worked on design/development of the new modules.`,
+    ],
+    tags: ["Java Design Patterns", "Web Services", "Java", "MYSQL", "SQL", "Spring"],
+    icon: "globe",
+  },
+   {
+    title: "IGate Corporation, India",
+    duration: "Dec 2012 - May 2014",
+    subtitle: "Technical Lead",
+    details: [
+      `Responsible for assessing and understanding the requirements and preparing the detailed design document and Unit Test cases for the e-banking application of Union Bank of California.`,
+      `Worked on the complete enhancement of Electronic Banking Alerts module.`,
+      `Lead the migration projects such as ANT to Maven and Jboss 4.3 to Jboss 5.1.`,
+      `Code review of the code developed by the team members.`,
+      `Worked on the development of automated test cases using Selenium.`,
+      `Responsible for Onsite-Offshore team co-ordination and mentoring team members.`,
+    ],
+    tags: ["Core Java", "JSF", "Spring", "Ajax", "SQL","Oracle 10g/11g"],
+    icon: "globe",
+  },
+   {
+    title: "TATA Consultancy Services Limited, India",
+    duration: "Jan 2007 - Jul 2012",
+    subtitle: "Module Lead",
+    details: [
+      `Worked on an Integrated Banking solution product, TCS BaNCS – which encompasses the functionality required for Securities Processing and Treasury Operations.`,
+      `Responsible for the end-to-end implementation and delivery of modules such as Foreign Exchange, Money Market and Advisory Management.`,
+      `Worked on design/development of the new modules.`,
+      `Assessing and understanding the requirements and preparing the detailed technical specification and database design for development.`,
+      `Resolving the cases for performance optimization of the code.`,
+ 
+    ],
+    tags: ["JSP", "JavaScript", "Struts", "Core Java", "SQL", "Oracle 10g/11g"],
+    icon: "globe",
+  },
+];
+
+export const education = [
+  {
+    title: "MSc, BigData and Business Analytics",
+    duration: "",
+    subtitle: "ESCP Business School, Paris",
+    details: [],
+    tags: ["Business Analytics", "Machine Learning", "Statistics", "Financial Analytics", "Consumer Insights","Data Mining"],
+    icon: "graduation-cap",
+  },
+  {
+    title: "Bachelors in Computer Science and Engineering",
+    duration: "2002 - 2006",
+    subtitle: "Adichunchanagiri Institute of Technology, Chickmagalur",
+    details: [],
+    tags: [
+      "Data Structures & Algorithms",
+      "Operating Systems",
+      "Database Management System",
+      "Computer Networks",
+      "Compiler Designing",
+      "Cloud Computing",
+    ],
+    icon: "graduation-cap",
+  },
+  
+];
+
+export const footer = [
+//   {
+//     label: "Dev Profiles",
+//     data: [
+//       {
+//         text: "Stackoverflow",
+//         link: "https://stackoverflow.com/users/8461233/xxxxx",
+//       },
+//       {
+//         text: "GitHub",
+//         link: "https://github.com/xxxxx",
+//       },
+//       {
+//         text: "LeetCode",
+//         link: "https://leetcode.com/xxxxx/",
+//       },
+//     ],
+//   },
+//   {
+//     label: "Resources",
+//     data: [
+//       {
+//         text: "Enable Dark/Light Mode",
+//         func: "enableDarkMode()",
+//       },
+//       {
+//         text: "Print this page",
+//         func: "window.print()",
+//       },
+//       {
+//         text: "Clone this page",
+//         link: "https://github.com/xxxx/xxxxx.github.io",
+//       },
+//     ],
+//   },
+  {
+    label: "Resources",
+    data: [
+    //   {
+    //     text: "Linkedin",
+    //     link: "https://www.linkedin.com/in/xxxx/",
+    //   },
+    //   {
+    //     text: "Twitter",
+    //     link: "https://twitter.com/thesigmakid",
+    //   },
+      {
+        text: "LinkedIn",
+        link: "https://www.linkedin.com/in/deepashree-bhat-3b432756/",
+        fontAwesomeIcon: "fab fa-linkedin", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+        backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+      },
+      {
+        text: "GitHub",
+        link: "https://github.com/deepashreebhat/deepashreebhat.github.io",
+        fontAwesomeIcon: "fab fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+        backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+      },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: [
+      "Made by Deepashree Bhat.",
+      //"&copy; No Copyrights. Feel free to use this template.",
+    ],
+  },
+];
